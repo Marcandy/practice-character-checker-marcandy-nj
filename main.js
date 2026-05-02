5dc1e2ff800b9882f0c5bbe2302b776606c34060
Marcandy <marcandy.nj@gmail.com>
2026-05-02 16:43:45 -0400

    added comments

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync");
 
-const userInput = readline.question("Hello, please name a fruit ");
-const idxNumber = readline.questionInt("Choose an index number zero - nine ");
+const userInput = readline.question("Hello, please name a fruit ");//ask for a string
+const idxNumber = readline.questionInt("Choose an index number zero - nine "); // ask for a number not to big
 
-const charString = userInput[idxNumber];
+const charString = userInput[idxNumber]; // access the string character based on the user input
 console.log("Your character is: " + charString);
